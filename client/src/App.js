6db2b948ed1755ab5b3a081885290068e35b84ff
Don Sneddon <don@Dons-MacBook-Pro.local>
2023-10-28 17:24:03 -0700

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    //fetch("http://localhost:4000")
+    fetch("https://psg-mern.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
